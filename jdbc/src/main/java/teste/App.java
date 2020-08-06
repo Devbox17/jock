0272ab382908dd3c01c8cfbc14581af140f680e5
Devbox17 <devboxever17@gmail.com>
2020-08-05 21:23:07 -0300

    vernion

---
+++ jdbc/src/main/java/teste/App.java
@@ -0,0 +1,13 @@
+package teste;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    int a, b;
+
+    public int soma(int a, int b) {
+        return a + b;
+    }
+}
---
+++ jdbc/src/test/java/teste/AppTest.java
@@ -0,0 +1,15 @@
+package teste;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public static void main(String[] args) {
+        App a = new App();
+
+        System.out.println(a.soma(2, 3));
+    }
+}
